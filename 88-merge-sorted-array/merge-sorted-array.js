963d31a2fd475206f4650f0ac2a179dbcb0dfde8
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-01 01:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (51.39%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    numsOneCopy = [];
+    if(n==0) return nums1;
+    for (let i = 0; i < m; i++) {
+        numsOneCopy[i] = nums1[i];
+    }
+    let p1=0;
+    let p2=0;
+    for (let i = 0; i < m + n; i++) {
+        if(numsOneCopy[p1]<nums2[p2] || p2>=nums2.length){
+            nums1[i] = numsOneCopy[p1];
+            p1++;
+        }
+        else{
+            nums1[i]=nums2[p2];
+            p2++;
+        }
+    }
+};
