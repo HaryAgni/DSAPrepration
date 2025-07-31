846f62886745e80cdebe4688b2e08596681202c5
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-01 01:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (92.52%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,11 +6,10 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    // numsOneCopy = nums1.slice(0,m);
     let p1 = m - 1;
     let p2 = n - 1;
     for (let i = m + n - 1; i >= 0; i--) {
-        if (nums1[p1] > nums2[p2] || p2 < 0) {
+        if ((p1>=0 && nums1[p1] > nums2[p2]) || p2 < 0) {
             nums1[i] = nums1[p1];
             p1--;
         }
