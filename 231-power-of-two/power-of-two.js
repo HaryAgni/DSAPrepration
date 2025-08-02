4416511026cfc1dca5527228c2cba6d9d2d3994c
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-02 23:17:36 +0530

    Time: 2 ms (7.20%) | Memory: 55.7 MB (91.07%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -5,8 +5,7 @@
 
 var isPowerOfTwo = function(n) {
     if(n==1) return true;
-    if(n/2==1) return true;
-    if(n/2<1) return false
+    if(n<1) return false
 
     return  isPowerOfTwo(n/2);
 };
