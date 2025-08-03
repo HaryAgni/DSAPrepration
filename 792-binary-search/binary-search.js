257ff4a35db11a8e0a63ba3cc2c61bea6381a666
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-03 21:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (77.17%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -5,7 +5,7 @@
  */
 var search = function (nums, target) {
     let start = 0;
-    let end = nums.length-1;
+    let end = nums.length - 1;
     while (start <= end) {
         let mid = Math.floor((start + end) / 2);
         if (nums[mid] == target) return mid;
