bd281b880d8e7644657cac068fb1fb4ea1f35621
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-05 16:44:54 +0530

    Time: 129 ms (49.08%) | Memory: 80.9 MB (50.00%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+    if (nums.length <= 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = sortArray(nums.slice(0, mid));
+    let right = sortArray(nums.slice(mid));
+    return merge(left, right);
+};
+
+var merge = function (arr1, arr2) {
+    let res = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            res.push(arr1[i]);
+            i++;
+        }
+        else {
+            res.push(arr2[j]);
+            j++;
+        }
+    }
+    return [...res, ...arr1.slice(i), ...arr2.slice(j)];
+}
