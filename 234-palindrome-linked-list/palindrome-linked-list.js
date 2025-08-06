8fa6ec02d74d6aefc51172da491c398dce39310f
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-06 20:37:05 +0530

    Time: 5 ms (62.01%) | Memory: 75 MB (96.29%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -10,17 +10,27 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-    let arr = [];
-    let curr = head;
-    while (curr) {
-        arr.push(curr.val);
-        curr = curr.next;
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    let mid = Math.floor(arr.length / 2);
-    for (i = 0; i < mid; i++) {
-        if (arr[i] !== arr[arr.length - 1 - i]) {
-            return false;
-        }
+
+    let prev = null;
+    let curr = slow;
+    while (curr) {
+        let temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    let p1 = head;
+    let p2 = prev;
+    while (p2) {
+        if (p1.val !== p2.val) return false;
+        p1 = p1.next;
+        p2 = p2.next;
     }
     return true;
 };
