c9e5a2db28be9a6192b9073929b412bff0625591
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-07 19:48:02 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (68.31%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -11,7 +11,7 @@
  */
 var deleteDuplicates = function(head) {
     let curr = head;
-    while(head && curr.next){
+    while(curr && curr.next){
         if(curr.val == curr.next.val){
             curr.next = curr.next.next;
         }
