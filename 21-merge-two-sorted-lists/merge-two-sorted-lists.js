36565456e322b55af9ba56731ae6b992f5c4bf19
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-08 17:50:09 +0530

    Time: 2 ms (14.39%) | Memory: 56.5 MB (81.14%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let head = new ListNode();
+    let node = head;
+    let p1 = list1;
+    let p2 = list2;
+    while (p1 || p2) {
+        if ((p1 && p2 && p1.val < p2.val) || !p2) {
+            node.next = new ListNode(p1.val);
+            p1 = p1.next;
+        } else {
+            node.next = new ListNode(p2.val);
+            p2 = p2.next;
+        }
+        node = node.next;
+    }
+    return head.next;
+};
