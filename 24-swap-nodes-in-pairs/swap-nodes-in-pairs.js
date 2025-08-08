a750ed22aa6a6c78238792cc4f1681f506128e27
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-09 00:02:36 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (65.68%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,37 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
+var swapPairs = function (head) {
     if(!head || !head.next) return head;
-    let l1 = head;
-    let l2 = head.next;
-    let p1 = l1;
-    let p2 = l2;
-    while(p2){
-        p1.next = p2.next;
-        p1 = p2;
-        p2 = p2.next;
+    let node = new ListNode();
+    node.next = head;
+    let prev = node;
+    let curr = head;
+    let next = head.next;
+    while(curr && curr.next){
+        curr.next = next.next;
+        next.next = curr;
+        prev.next = next;
+        prev = curr;
+        curr = curr.next;
+        next = curr && curr.next;
     }
-    let flag = true;
-    p1 = l1;
-    p2 = l2;
-    let newhead = new ListNode();
-    let node = newhead;
-    while(p1 && p2){
-        if(flag){
-            node.next = p2;
-            p2 = p2.next;
-        }
-        else{
-            node.next = p1;
-            p1=p1.next
-        }
-        flag = !flag;
-        node = node.next
-
-    }
-    node.next = p1 || p2;
-
-    return l2;
+    return node.next;
 
 };
