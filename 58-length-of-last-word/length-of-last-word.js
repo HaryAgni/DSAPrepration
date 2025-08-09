6ca4dd3500e9c31d828a01dd90017691cf01031e
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-09 23:05:55 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (57.16%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,28 +3,12 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    let endIndex = s.length - 1;
-    let startIndex = 0;
-    for (i = s.length - 1; i > 0; i--) {
-        if (s[i] == " " && s[i - 1] !== " ") {
-            endIndex = i - 1;
-        } if (s[i] !== " " && s[i - 1] == " ") {
-            startIndex = i;
-            break;
-        }
+    let end = s.length - 1;
+    let count = 0;
+    while (end >= 0) {
+        if (s[end] !== " ") count++;
+        else if (count > 0) break;
+        end--;
     }
-    return endIndex - startIndex + 1;
-    // let wordIndex = 0;
-    // for (i = 0; i < s.length - 1; i++) {
-    //     if (s[i] == " " && s[i + 1] !== " ") {
-    //         wordIndex = i + 1;
-    //     }
-    // }
-    // let length = 0;
-    // let newStr = s.slice(wordIndex);
-    // for (i = 0; i < newStr.length && newStr[i] !== " "; i++) {
-    //     length++;
-    // }
-    // return length;
-
+    return count;
 };
