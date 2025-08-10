2fb2b624e477e14b16877bc12f7721b749582090
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-10 21:34:58 +0530

    Time: 8 ms (58.22%) | Memory: 56.4 MB (66.24%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -6,21 +6,17 @@ var maxFreqSum = function (s) {
     let vowels = new Set("aeiou");
     let obj = {};
     for (let i = 0; i < s.length; i++) {
-        if (obj[s[i]]) {
-            obj[s[i]]++;
-        }
-        else {
-            obj[s[i]] = 1;
-        }
+        obj[s[i]] = obj[s[i]] ? ++obj[s[i]] : 1;
     }
     let vowelMax = 0;
     let consonantMax = 0;
-    for (i = 0; i < s.length; i++) {
-        if (vowels.has(s[i])) {
-            vowelMax = Math.max(vowelMax, obj[s[i]]);
+    let objKeys = Object.keys(obj);
+    for (i = 0; i < objKeys.length; i++) {
+        if (vowels.has(objKeys[i])) {
+            vowelMax = Math.max(vowelMax, obj[objKeys[i]]);
         }
         else {
-            consonantMax = Math.max(consonantMax, obj[s[i]]);
+            consonantMax = Math.max(consonantMax, obj[objKeys[i]]);
         }
     }
     return vowelMax + consonantMax;
