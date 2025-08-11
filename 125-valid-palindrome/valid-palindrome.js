3cc27caf05f381a89c07840fa866ea8781155774
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-11 20:34:15 +0530

    Time: 5 ms (64.27%) | Memory: 57.9 MB (58.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,7 +6,6 @@ var isPalindrome = function (s) {
     let str = s.toLowerCase().replace(/[^a-z0-9]/g,"");
     let l = 0;
     let r = str.length - 1;
-    console.log(str);
     while (l < r) {
         if (str[l] !== str[r]) return false;
         l++;
