ceae6541fa511f37163175b188897f92e1f452d7
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-12 21:41:33 +0530

    Time: 8 ms (32.61%) | Memory: 54.5 MB (76.96%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -8,9 +8,13 @@ var isIsomorphic = function (s, t) {
     let obj = {};
     let set = new Set();
     for (let i = 0; i < s.length; i++) {
-        if ((obj[s[i]] && obj[s[i]] !== t[i]) || (!obj[s[i]] && set.has(t[i]))) return false;
-        obj[s[i]] = t[i];
-        set.add(t[i]);
+        if (obj[s[i]] && obj[s[i]] !== t[i]) return false;
+        else {
+            if (!obj[s[i]] && set.has(t[i])) return false;
+            obj[s[i]] = t[i];
+            set.add(t[i]);
+        }
+
     }
     return true;
 };
