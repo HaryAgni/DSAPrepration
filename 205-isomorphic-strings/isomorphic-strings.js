acc969064a160835149b081bdf329980a0b33286
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-12 21:16:53 +0530

    Time: 6 ms (66.82%) | Memory: 55.2 MB (45.29%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+    let obj = {};
+    let set = new Set();
+    for (let i = 0; i < s.length; i++) {
+        if ((obj[s[i]] && obj[s[i]] !== t[i]) || (!obj[s[i]] && set.has(t[i]))) return false;
+        obj[s[i]] = t[i];
+        set.add(t[i]);
+    }
+    return true;
+};
