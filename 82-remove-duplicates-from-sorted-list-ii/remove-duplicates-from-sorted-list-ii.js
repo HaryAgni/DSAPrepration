cc0af0d5884873d169aa9260f2c611ac19914b47
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-12 14:50:15 +0530

    Time: 3 ms (11.04%) | Memory: 56.3 MB (93.22%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head || !head.next) return head;
+    let repeatedNode = new ListNode(-Infinity);
+    let node = new ListNode();
+    node.next = head;
+    let prev = node;
+    let curr = head;
+    let next = head.next;
+    while (curr && next) {
+        if (curr.val == next.val || repeatedNode.val == curr.val) {
+            repeatedNode = curr;
+            next = next.next;
+            curr = curr.next;
+        }
+        else {
+            prev.next = curr;
+            prev = curr;
+            curr = next;
+            next = next.next
+        }
+
+    }
+    prev.next = repeatedNode.val !== curr.val ? curr : null;
+    return node.next;
+};
