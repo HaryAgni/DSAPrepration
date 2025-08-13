cbb90121e91d2e9a9c867f85ca77f5c3488d98ce
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-13 18:01:18 +0530

    Time: 57 ms (10.53%) | Memory: 68.7 MB (31.83%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -5,9 +5,18 @@
 var groupAnagrams = function (strs) {
     let map = {};
     for (let i = 0; i < strs.length; i++) {
-        let sortedStr = strs[i].split("").sort().join("");
-        if (map[sortedStr]) map[sortedStr].push(strs[i]);
-        else map[sortedStr] = [strs[i]];
+        let currStr = strs[i];
+        let freqArr = Array(26).fill(0);
+        for (let j = 0; j < currStr.length; j++) {
+            let index = currStr[j].charCodeAt() - 'a'.charCodeAt();
+            freqArr[index] = ++freqArr[index];
+        }
+        let key = "";
+        for (let k = 0; k < 26; k++) {
+            key = key + String.fromCharCode(k) + freqArr[k];
+        }
+        if (map[key]) map[key].push(currStr);
+        else map[key] = [currStr];
     }
     return Object.values(map);
 };
