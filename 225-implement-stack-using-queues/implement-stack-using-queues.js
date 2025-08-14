327c7ac6724d0805c82d90446103843c44bffda0
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-14 18:43:38 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (79.76%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -1,6 +1,6 @@
 
 var MyStack = function () {
-    this.q1 = [];
+    this.q = [];
 };
 
 /** 
@@ -8,18 +8,18 @@ var MyStack = function () {
  * @return {void}
  */
 MyStack.prototype.push = function (x) {
-    this.q1.push(x);
+    this.q.push(x);
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.pop = function () {
-    let n = this.q1.length;
+    let n = this.q.length;
     for (let i = 0; i < n - 1; i++) {
-        this.q1.push(this.q1.shift());
+        this.q.push(this.q.shift());
     }
-    return this.q1.shift();
+    return this.q.shift();
 
 };
 
@@ -27,12 +27,12 @@ MyStack.prototype.pop = function () {
  * @return {number}
  */
 MyStack.prototype.top = function () {
-    let n = this.q1.length;
+    let n = this.q.length;
     for (let i = 0; i < n - 1; i++) {
-        this.q1.push(this.q1.shift());
+        this.q.push(this.q.shift());
     }
-    let res = this.q1.shift();
-    this.q1.push(res);
+    let res = this.q.shift();
+    this.q.push(res);
     return res;
 };
 
@@ -40,7 +40,7 @@ MyStack.prototype.top = function () {
  * @return {boolean}
  */
 MyStack.prototype.empty = function () {
-    return this.q1.length == 0;
+    return this.q.length == 0;
 };
 
 /** 
