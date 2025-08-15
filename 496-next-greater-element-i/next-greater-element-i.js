9c26270274557185d2f2c17788a794aad446c7d7
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-15 21:30:33 +0530

    Time: 2 ms (87.96%) | Memory: 58.3 MB (19.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -7,26 +7,17 @@ var nextGreaterElement = function (nums1, nums2) {
     let map = {};
     let stack = [];
     for (let i = nums2.length - 1; i >= 0; i--) {
-        if (stack.length !== 0) {
-            let temp = stack[stack.length - 1];
-            while (stack.length !== 0) {
-                if (temp > nums2[i]) {
-                    stack.push(nums2[i]);
-                    map[nums2[i]] = temp;
-                    break;
-                }
-                stack.pop();
-                temp = stack[stack.length - 1];
+        let temp = stack[stack.length - 1];
+        while (stack.length) {
+            if (temp > nums2[i]) {
+                map[nums2[i]] = temp;
+                break;
             }
+            stack.pop();
+            temp = stack[stack.length - 1];
         }
-        if (stack.length == 0) {
-            stack.push(nums2[i]);
-            map[nums2[i]] = -1;
-        }
+        if (stack.length == 0) map[nums2[i]] = -1;
+        stack.push(nums2[i]);
     }
-    let res = [];
-    for (let i = 0; i < nums1.length; i++) {
-        res.push(map[nums1[i]]);
-    }
-    return res;
+    return nums1.map(i => map[i]);
 };
