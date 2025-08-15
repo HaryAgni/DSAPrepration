6fe6f5d15115f42e17ed391cf322bec2d373d38f
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-15 21:08:51 +0530

    Time: 15 ms (6.04%) | Memory: 57.1 MB (57.28%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,23 +4,30 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-    let res = [];
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums1[i] == nums2[nums2.length - 1]) res.push(-1);
-        else {
-            let index = 0;
-            while (nums2[index] !== nums1[i]) {
-                index++;
-            }
-            let temp = -1;
-            for (let j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    temp = nums2[j];
+    let map = {};
+    let stack = [];
+    for (let i = nums2.length - 1; i >= 0; i--) {
+        if (stack.length !== 0) {
+            let temp = stack[stack.length - 1];
+            while (stack.length !== 0) {
+                if (temp > nums2[i]) {
+                    stack.push(nums2[i]);
+                    map[nums2[i]] = temp;
                     break;
                 }
+                stack.pop();
+                temp = stack[stack.length - 1];
             }
-            res.push(temp);
         }
+        if (stack.length == 0) {
+            stack.push(nums2[i]);
+            map[nums2[i]] = -1;
+        }
+    }
+    console.log(map);
+    let res = [];
+    for (let i = 0; i < nums1.length; i++) {
+        res.push(map[nums1[i]]);
     }
     return res;
 };
