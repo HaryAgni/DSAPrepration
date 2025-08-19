9b9b8a4dd10b67c19c0a1300541f2e6798d17953
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-19 14:09:37 +0530

    Time: 14 ms (7.37%) | Memory: 74.2 MB (5.26%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var zeroFilledSubarray = function (nums) {
+    let count = 0;
+    let streak = 0;
+    for (let num of nums) {
+        if (num === 0) {
+            streak++;
+            count += streak;
+        } else streak = 0;
+
+    }
+    return count;
+};
