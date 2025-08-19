80ade3982dc77e237399054e9c9fa70a2ad528db
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-19 23:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (28.56%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -7,12 +7,12 @@ var singleNonDuplicate = function (nums) {
     let end = nums.length - 1;
     while (start <= end) {
         let mid = start + Math.floor((end - start) / 2);
-        if (nums[mid] == nums[mid + 1]) {
+        if (mid > 0 && mid < nums.length - 1 && nums[mid] == nums[mid + 1]) {
             let leftCount = mid - start;
             if (leftCount % 2 == 1) end = mid - 1;
             else start = mid + 2;
         }
-        else if (nums[mid] == nums[mid - 1]) {
+        else if (mid > 0 && mid < nums.length - 1 && nums[mid] == nums[mid - 1]) {
             let leftCount = mid - 1 - start;
             if (leftCount % 2 == 1) end = mid - 2;
             else start = mid + 1;
