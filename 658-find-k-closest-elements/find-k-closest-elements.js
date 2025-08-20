1e2a87659926c7ade496c2c23f5446be6c9f679c
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-20 18:11:24 +0530

    Time: 0 ms (100.00%) | Memory: 61.8 MB (52.25%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+    if (k == arr.length) return arr;
+    let start = 0;
+    let end = arr.length - 1;
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (mid + k < arr.length && (arr[mid + k] - x) < (x - arr[mid])) {
+            start = mid + 1;
+        }
+        else end = mid;
+    }
+
+    return arr.slice(start, start + k);
+};
