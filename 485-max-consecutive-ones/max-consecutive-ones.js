187dac538b8e24d415e052cbf0f747f40422ba04
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-20 19:58:25 +0530

    Time: 1 ms (90.68%) | Memory: 59.5 MB (73.84%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -3,18 +3,11 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
-    let currentCount = 0;
+    let currCount = 0;
     let maxCount = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == 0) {
-            currentCount = 0;
-        }
-        else {
-            currentCount++;
-            if (currentCount > maxCount) {
-                maxCount = currentCount;
-            }
-        }
+        if (nums[i] !== 1) currCount = 0;
+        else maxCount = Math.max(maxCount, ++currCount);
     }
     return maxCount;
 };
