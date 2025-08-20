cd072390e1401061008821c54d0ddc07ca8d9abd
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-20 21:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 56.2 MB (18.49%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let key = target - nums[i];
+        if (key in map) return [map[key], i];
+        map[nums[i]] = i;
+    }
+};
