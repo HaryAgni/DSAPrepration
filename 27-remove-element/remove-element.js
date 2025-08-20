ea6488f9d38688a5216503b69d8f659176b91149
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-20 19:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (50.35%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -6,14 +6,9 @@
 var removeElement = function (nums, val) {
     let prev = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[prev] == val && nums[i] !== val) {
-            let temp = nums[i];
-            nums[i] = nums[prev];
-            nums[prev] = temp;
+        if (nums[i] !== val) {
+            nums[prev] = nums[i];
             prev++;
-        }
-        else if (nums[i] !== val) {
-            prev++
         }
     }
     return prev;
