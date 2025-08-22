53523df602ce8ce51f7140a803659ff22ef43ac5
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-22 18:22:33 +0530

    Time: 10 ms (35.18%) | Memory: 57.7 MB (31.62%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -4,21 +4,17 @@
  */
 var maxFreqSum = function (s) {
     let set = new Set('aeiou');
-    let vowels = {};
-    let consonant = {};
+    let map = {};
     for (let i = 0; i < s.length; i++) {
-        if (set.has(s[i])) vowels[s[i]] = vowels[s[i]] ? ++vowels[s[i]] : 1;
-        else consonant[s[i]] = consonant[s[i]] ? ++consonant[s[i]] : 1;
+        map[s[i]] = map[s[i]] ? ++map[s[i]] : 1;
     }
     let vMax = 0;
     let cMax = 0;
-    let vKeys = Object.keys(vowels);
-    let cKeys = Object.keys(consonant);
-    for (let i = 0; i < vKeys.length; i++) {
-        vMax = Math.max(vMax, vowels[vKeys[i]]);
+    let keys = Object.keys(map)
+    for (let i = 0; i < keys.length; i++) {
+        if (set.has(keys[i])) vMax = Math.max(vMax, map[keys[i]])
+        else cMax = Math.max(cMax, map[keys[i]])
     }
-    for (let i = 0; i < cKeys.length; i++) {
-        cMax = Math.max(cMax, consonant[cKeys[i]]);
-    }
+
     return vMax + cMax;
 };
