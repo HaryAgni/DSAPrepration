37f70efb76a9412df1ba64b32c776d41920ca050
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-22 19:36:45 +0530

    Time: 1 ms (81.98%) | Memory: 58.9 MB (48.50%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -3,10 +3,8 @@
  * @return {string}
  */
 var largestOddNumber = function (num) {
-    let r = num.length - 1;
-    while (r >= 0) {
-        if (Number(num[r]) % 2 == 0) r--;
-        else return num.substring(0, r + 1);
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (Number(num[i]) % 2 == 1) return num.substring(0, i + 1);
     }
     return "";
 };
