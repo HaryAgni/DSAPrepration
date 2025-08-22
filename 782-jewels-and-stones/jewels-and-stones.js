9ebc5322f6e2fc30fa026c40952cb77164a9193a
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-22 17:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (83.98%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,10 +4,10 @@
  * @return {number}
  */
 var numJewelsInStones = function (jewels, stones) {
+    let set = new Set(jewels);
     let count = 0;
-    let jSet = new Set(jewels);
-    for (i = 0; i < stones.length; i++) {
-        if (jSet.has(stones[i])) count++;
+    for (let i = 0; i < stones.length; i++) {
+        if (set.has(stones[i])) count++;
     }
     return count;
 };
