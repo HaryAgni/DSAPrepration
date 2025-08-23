36be46fbacdbe34d8c43788027785d36785f5e01
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-23 16:44:18 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (63.36%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -15,25 +15,22 @@ MyStack.prototype.push = function (x) {
  * @return {number}
  */
 MyStack.prototype.pop = function () {
-    let n = this.q.length;
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < this.q.length - 1; i++) {
         this.q.push(this.q.shift());
     }
     return this.q.shift();
-
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.top = function () {
-    let n = this.q.length;
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < this.q.length - 1; i++) {
         this.q.push(this.q.shift());
     }
-    let res = this.q.shift();
-    this.q.push(res);
-    return res;
+    let temp = this.q.shift();
+    this.q.push(temp);
+    return temp;
 };
 
 /**
