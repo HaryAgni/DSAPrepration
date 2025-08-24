3f8b6c02ba654208976d8e458fcebe6d0dd018f9
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-24 21:00:08 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (42.94%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,29 +4,28 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-    if (nums.length == 1 && nums[0] == target) return [0, 0];
     let start = 0;
     let end = nums.length - 1;
     let res = [-1, -1];
-    while (start < end) {
+    while (start <= end) {
         let mid = start + Math.floor((end - start) / 2);
-        if (nums[mid] >= target) end = mid;
-        else start = mid + 1;
-        if (nums[start] == target) {
-            res[0] = start;
-            break;
+        if (nums[mid] == target) {
+            res[0] = mid;
+            end = mid - 1;
         }
+        else if (nums[mid] < target) start = mid + 1;
+        else end = mid - 1;
     }
     start = 0;
     end = nums.length - 1;
-    while (start < end) {
+    while (start <= end) {
         let mid = start + Math.ceil((end - start) / 2);
-        if (nums[mid] <= target) start = mid;
-        else end = mid - 1;
-        if (nums[end] == target) {
-            res[1] = end;
-            break;
+        if (nums[mid] == target) {
+            res[1] = mid;
+            start = mid + 1;
         }
+        else if (nums[mid] < target) start = mid + 1;
+        else end = mid - 1;
     }
     return res;
 };
