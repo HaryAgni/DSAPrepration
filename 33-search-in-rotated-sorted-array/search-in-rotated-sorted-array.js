6dc8152847c4acc3870854200de77a21b62e5496
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-24 19:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (74.45%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -8,24 +8,15 @@ var search = function (nums, target) {
     let end = nums.length - 1;
     while (start <= end) {
         let mid = Math.floor((start + end) / 2);
-        if (target == nums[mid]) return mid;
+        if (nums[mid] == target) return mid;
         else if (nums[start] <= nums[mid]) {
-            if (target >= nums[start] && target < nums[mid]) {
-                end = mid - 1;
-            }
-            else {
-                start = mid + 1;
-            }
+            if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
         }
         else {
-            if (target > nums[mid] && target <= nums[end]) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
+            if (target > nums[mid] && target <= nums[end]) start = mid + 1;
+            else end = mid - 1;
         }
     }
     return -1;
-
 };
