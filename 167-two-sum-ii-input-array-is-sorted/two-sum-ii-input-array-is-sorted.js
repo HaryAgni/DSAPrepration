8bc896ec0708de244e0a85a13195657a8ccc5194
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-26 21:13:51 +0530

    Time: 2 ms (62.71%) | Memory: 55.9 MB (58.64%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        let sum = nums[start] + nums[end];
+        if (sum > target) end--;
+        else if (sum < target) start++;
+        else return [start + 1, end + 1];
+    }
+};
