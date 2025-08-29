f153bfffcd4ee2edc455059b18a05564aa797a4e
Harshit Agnihotri <haryagni123@gmail.com>
2025-08-29 23:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (33.33%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.js
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 var flowerGame = function (n, m) {
+    
     let oddN = Math.floor((n + 1) / 2);
     let evenN = Math.floor(n / 2);
     let oddM = Math.floor((m + 1) / 2);
