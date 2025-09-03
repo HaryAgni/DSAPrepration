b98d703dbc78b9d6ed4e4a13822c550a5087645e
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-03 19:50:23 +0530

    Time: 21 ms (13.46%) | Memory: 61.7 MB (9.10%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let n = height.length;
+    let maxL = [height[0]];
+    let maxR = [];
+    maxR[n - 1] = height[n - 1];
+    for (let i = 1; i < n; i++) {
+        maxL[i] = Math.max(maxL[i - 1], height[i]);
+        maxR[n - 1 - i] = Math.max(height[n - 1 - i], maxR[n - i]);
+    }
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        ans = ans + (Math.min(maxL[i], maxR[i]) - height[i]);
+    }
+    return ans;
+};
