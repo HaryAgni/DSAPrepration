2912dc9bf1e102cab2fa422a1441272bd2b4e949
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-04 22:44:36 +0530

    Time: 18 ms (26.75%) | Memory: 59.9 MB (25.56%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -8,9 +8,7 @@ var lengthOfLongestSubstring = function (s) {
     let i = 0;
     let j = 0;
     while (j < s.length) {
-        if (s[j] in map && map[s[j]] >= i) {
-            i = map[s[j]] + 1;
-        }
+        if (s[j] in map && map[s[j]] >= i) i = map[s[j]] + 1;
         map[s[j]] = j;
         maxWindow = Math.max(maxWindow, j - i + 1);
         j++;
