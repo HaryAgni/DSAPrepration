7db54f9b7073a47e2b3ff1b13f5a21fd4609fdfa
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-04 22:57:46 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (63.64%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function (x, y, z) {
+    const dist1 = Math.abs(x - z);
+    const dist2 = Math.abs(y - z);
+    if (dist1 < dist2) {
+        return 1;
+    } else if (dist2 < dist1) {
+        return 2;
+    } else {
+        return 0;
+    }
+};
