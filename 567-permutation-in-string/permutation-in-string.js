90cdb131cfc8988a04bf545ef8db8dcc996a0ffb
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-06 17:00:26 +0530

    Time: 12 ms (67.95%) | Memory: 57.8 MB (61.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -4,25 +4,30 @@
  * @return {boolean}
  */
 var checkInclusion = function (s1, s2) {
-    let str1 = createStr(s1);
-    for (let i = 0; i <= s2.length - s1.length; i++) {
-        let j = i + s1.length;
-        let k = i;
-        let currStr = "";
-        while (k < j) {
-            currStr += s2[k];
-            k++;
-        }
-        let str2 = createStr(currStr);
-        if (str1 == str2) return true;
+    let hashA = Array(26).fill(0);
+    let hashB = Array(26).fill(0);
+    let n = s1.length;
+    let m = s2.length;
+    let base = 'a'.charCodeAt(0);
+    for (let i = 0; i < n; i++) {
+        hashA[s1.charCodeAt(i) - base]++;
+        hashB[s2.charCodeAt(i) - base]++;
+    }
+
+    let i = 0;
+    let j = i + n - 1;
+    while (j < m) {
+        if (isHashSame(hashA, hashB)) return true;
+        hashB[s2.charCodeAt(i) - base]--;
+        i++;
+        j++;
+        hashB[s2.charCodeAt(j) - base]++;
     }
     return false;
 };
-
-var createStr = function (str) {
-    let freqArr = Array(26).fill(0);
-    for (let i = 0; i < str.length; i++) {
-        freqArr[str.charCodeAt(i) - 97]++;
+var isHashSame = function (hashA, hashB) {
+    for (let i = 0; i < 26; i++) {
+        if (hashA[i] !== hashB[i]) return false;
     }
-    return freqArr.join('#');
+    return true;
 }
