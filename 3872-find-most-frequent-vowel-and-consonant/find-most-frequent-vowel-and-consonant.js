51d32c1cdd29ef46e03a3808938fbc5f1fc07714
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-07 22:15:38 +0530

    Time: 6 ms (78.31%) | Memory: 58.5 MB (12.56%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -4,17 +4,17 @@
  */
 var maxFreqSum = function (s) {
     let set = new Set('aeiou');
-    let map = {};
-    for (let i = 0; i < s.length; i++) {
-        map[s[i]] = map[s[i]] ? ++map[s[i]] : 1;
-    }
     let vMax = 0;
     let cMax = 0;
-    let keys = Object.keys(map)
-    for (let i = 0; i < keys.length; i++) {
-        if (set.has(keys[i])) vMax = Math.max(vMax, map[keys[i]])
-        else cMax = Math.max(cMax, map[keys[i]])
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] = (map[s[i]] || 0) + 1;
+        if (set.has(s[i])) {
+            vMax = Math.max(vMax, map[s[i]]);
+        }
+        else {
+            cMax = Math.max(cMax, map[s[i]]);
+        }
     }
-
     return vMax + cMax;
 };
