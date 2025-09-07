d3b4e97c4bd9a45d2ed954be315c13c8cc623da8
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-07 20:14:12 +0530

    Time: 56 ms (65.64%) | Memory: 65.7 MB (6.34%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,13 +12,14 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function (headA, headB) {
-    let p1 = headA;
-    let p2 = headB;
-    while (p1 || p2) {
-        if (p1 == p2) return p1;
-        p1 = p1 ? p1.next : headB;
-        p2 = p2 ? p2.next : headA;
-
+    let set = new Set();
+    while (headA) {
+        set.add(headA);
+        headA = headA.next;
+    }
+    while (headB) {
+        if (set.has(headB)) return headB;
+        headB = headB.next;
     }
     return null;
 };
