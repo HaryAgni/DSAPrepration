d1c6c5bfc00122ba9d08946b87246b47d09a67e7
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-07 22:19:39 +0530

    Time: 10 ms (34.42%) | Memory: 58.5 MB (12.56%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -9,12 +9,8 @@ var maxFreqSum = function (s) {
     let map = {};
     for (let i = 0; i < s.length; i++) {
         map[s[i]] = (map[s[i]] || 0) + 1;
-        if (set.has(s[i])) {
-            vMax = Math.max(vMax, map[s[i]]);
-        }
-        else {
-            cMax = Math.max(cMax, map[s[i]]);
-        }
+        if (set.has(s[i])) vMax = Math.max(vMax, map[s[i]]);
+        else cMax = Math.max(cMax, map[s[i]]);
     }
     return vMax + cMax;
 };
