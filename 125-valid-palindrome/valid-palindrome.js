652096d52868eb0a30bdc860e03d5aef8536248c
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-08 20:30:02 +0530

    Time: 8 ms (29.90%) | Memory: 59.7 MB (34.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,17 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    let set = new Set('abcdefghijklmnopqrstuvwxyz1234567890');
-    let left = 0;
-    let right = s.length - 1;
-    while (left < right) {
-        if (!set.has(s[left].toLowerCase())) left++;
-        else if (!set.has(s[right].toLowerCase())) right--;
+    let set = new Set("abcdefghijklmnopqrstuvwxyz1234567890");
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+        if (!set.has(s[i].toLowerCase())) i++;
+        else if (!set.has(s[j].toLowerCase())) j--;
+        else if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
         else {
-            if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
-            left++;
-            right--;
+            i++;
+            j--;
         }
+
     }
     return true;
 };
