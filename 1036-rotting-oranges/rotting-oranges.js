30db42685cf7fd8064050283de954a00cdb10fc4
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-08 22:22:16 +0530

    Time: 8 ms (59.00%) | Memory: 58.9 MB (31.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -3,38 +3,37 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
-    let n = grid.length - 1;
-    let m = grid[0].length - 1;
+    let n = grid.length;
+    let m = grid[0].length;
     let q = [];
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= m; j++) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
             if (grid[i][j] == 2) q.push([i, j, 0]);
         }
     }
-
     let minutes = 0;
     while (q.length) {
-        let [x, y, currMinute] = q.shift();
-        if (x > 0 && grid[x - 1][y] == 1) {
+        let [x, y, min] = q.shift();
+        if (x - 1 >= 0 && grid[x - 1][y] == 1) {
             grid[x - 1][y] = 2;
-            q.push([x - 1, y, currMinute + 1]);
+            q.push([x - 1, y, min + 1]);
         }
-        if (x < n && grid[x + 1][y] == 1) {
+        if (x + 1 < n && grid[x + 1][y] == 1) {
             grid[x + 1][y] = 2;
-            q.push([x + 1, y, currMinute + 1]);
+            q.push([x + 1, y, min + 1]);
         }
-        if (y > 0 && grid[x][y - 1] == 1) {
+        if (y - 1 >= 0 && grid[x][y - 1] == 1) {
             grid[x][y - 1] = 2;
-            q.push([x, y - 1, currMinute + 1]);
+            q.push([x, y - 1, min + 1]);
         }
-        if (y < m && grid[x][y + 1] == 1) {
+        if (y + 1 < m && grid[x][y + 1] == 1) {
             grid[x][y + 1] = 2;
-            q.push([x, y + 1, currMinute + 1]);
+            q.push([x, y + 1, min + 1]);
         }
-        minutes = currMinute;
+        minutes = min;
     }
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= m; j++) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
             if (grid[i][j] == 1) return -1;
         }
     }
