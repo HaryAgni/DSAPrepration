c3ecea4f6feb2eba72f3de99b5cb775266f66e30
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-08 20:50:24 +0530

    Time: 2 ms (42.72%) | Memory: 55 MB (43.25%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -4,13 +4,12 @@
  */
 var longestCommonPrefix = function (strs) {
     let str = strs[0];
-    let pre = "";
     for (let i = 0; i < str.length; i++) {
         let curr = str[i];
         for (let j = 0; j < strs.length; j++) {
-            if (strs[j][i] !== curr || i >= strs[j].length) return pre;
+            if (strs[j][i] !== curr || i >= strs[j].length) return str.substring(0, i);
         }
-        pre += curr;
     }
-    return pre;
+    return str;
+
 };
