18d234ee024193daa867300108bddf56106a6491
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-08 23:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (14.39%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,26 +6,26 @@
 var searchRange = function (nums, target) {
     let start = 0;
     let end = nums.length - 1;
-    let res = [-1, -1];
+    let ans = [-1, -1];
     while (start <= end) {
         let mid = start + Math.floor((end - start) / 2);
         if (nums[mid] == target) {
-            res[0] = mid;
+            ans[0] = mid;
             end = mid - 1;
         }
-        else if (nums[mid] < target) start = mid + 1;
-        else end = mid - 1;
+        else if (nums[mid] > target) end = mid-1;
+        else start = mid + 1;
     }
-    start = 0;
+    start =0;
     end = nums.length - 1;
     while (start <= end) {
         let mid = start + Math.ceil((end - start) / 2);
         if (nums[mid] == target) {
-            res[1] = mid;
+            ans[1] = mid;
             start = mid + 1;
         }
-        else if (nums[mid] < target) start = mid + 1;
-        else end = mid - 1;
+        else if (nums[mid] > target) end = mid-1;
+        else start = mid+1;
     }
-    return res;
+    return ans;
 };
