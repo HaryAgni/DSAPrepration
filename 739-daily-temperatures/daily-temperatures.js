d815cda2d6d668e8bf7153b81bc8ea7f2f7123f8
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-08 21:47:26 +0530

    Time: 13 ms (92.97%) | Memory: 74 MB (99.80%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,18 +3,18 @@
  * @return {number[]}
  */
 var dailyTemperatures = function (arr) {
-    let res = Array(arr.length).fill(0);
     let stack = [];
+    let ans = Array(arr.length).fill(0);
     for (let i = arr.length - 1; i >= 0; i--) {
         while (stack.length) {
             let top = stack[stack.length - 1];
             if (arr[top] > arr[i]) {
-                res[i] = top - i;
+                ans[i] = top - i;
                 break;
             }
             stack.pop();
         }
         stack.push(i);
     }
-    return res;
+    return ans;
 };
