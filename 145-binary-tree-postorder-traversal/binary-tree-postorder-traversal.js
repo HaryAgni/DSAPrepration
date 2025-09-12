c9f2385404c90a873b1a5e8a99f939ce8dd42179
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-12 22:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (5.44%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -13,9 +13,7 @@
 var postorderTraversal = function (root) {
     let ans = [];
     function traversal(curr) {
-        if (!curr) {
-            return;
-        }
+        if (!curr) return;
         traversal(curr.left);
         traversal(curr.right);
         ans.push(curr.val);
