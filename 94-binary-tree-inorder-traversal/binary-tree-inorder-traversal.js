07ceba6cbc5318f0b338657e51f40c0aae8cb370
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-12 22:20:09 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (79.67%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -13,9 +13,7 @@
 var inorderTraversal = function (root) {
     let ans = [];
     function traversal(curr) {
-        if (!curr) {
-            return;
-        }
+        if (!curr) return;
         traversal(curr.left);
         ans.push(curr.val);
         traversal(curr.right);
