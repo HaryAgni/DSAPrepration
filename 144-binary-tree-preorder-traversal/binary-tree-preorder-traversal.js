efdfe75bfa0d752ccec1789d7d74f922b92f09c2
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-12 21:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (16.10%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -12,12 +12,14 @@
  */
 var preorderTraversal = function (root) {
     let ans = [];
+
     function traversal(curr) {
         if (!curr) return;
         ans.push(curr.val);
         traversal(curr.left);
         traversal(curr.right);
     }
+    
     traversal(root);
     return ans;
 };
