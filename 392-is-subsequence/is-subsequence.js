ab75bf7ed462cb33eeee911fb8b4fb06b1ba01a4
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-14 14:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (96.82%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,11 +4,9 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-    if (!s.length) return true;
     let i = 0;
     for (let j = 0; j < t.length; j++) {
         if (s[i] == t[j]) i++;
-        if (i == s.length) return true;
     }
-    return false;
+    return i == s.length;
 };
