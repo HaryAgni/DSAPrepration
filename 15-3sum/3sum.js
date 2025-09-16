45d2c8f7e69c63b5b8e30be7ded8c3f2d1efedc0
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-16 22:06:56 +0530

    Time: 30 ms (92.44%) | Memory: 71.9 MB (71.60%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,29 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    let ans = [];
     nums = nums.sort((a, b) => a - b);
+    let ans = [];
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > 0 || nums[nums.length - 1] < 0) break;
-        else if (i == 0 || nums[i] !== nums[i - 1]) {
+        if (i == 0 || nums[i - 1] !== nums[i]) {
             twoSum(i, nums, ans);
         }
     }
     return ans;
+
 };
 
-var twoSum = function (i, nums, ans) {
-    let l = i + 1;
+var twoSum = function (curr, nums, ans) {
+    let l = curr + 1;
     let r = nums.length - 1;
     while (l < r) {
-        let sum = nums[i] + nums[l] + nums[r];
+        let sum = nums[curr] + nums[l] + nums[r];
         if (sum > 0) r--;
         else if (sum < 0) l++;
         else {
-            ans.push([nums[i], nums[l], nums[r]]);
+            ans.push([nums[curr], nums[l], nums[r]]);
             l++;
             r--;
-            while (l < r && nums[l] == nums[l - 1]) l++;
+            while (nums[l] == nums[l - 1] && l < r) l++;
         }
+
     }
 }
