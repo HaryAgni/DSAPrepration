d081cf9b4716578e4405365acb27cd48627c7781
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-23 22:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 58.4 MB (38.58%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -14,5 +14,6 @@ var maxDepth = function (root) {
     if (!root) return 0;
     let leftMax = maxDepth(root.left);
     let rightMax = maxDepth(root.right);
+    
     return 1 + Math.max(leftMax, rightMax);
 };
