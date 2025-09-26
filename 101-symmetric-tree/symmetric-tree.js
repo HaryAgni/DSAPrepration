f65e0e9bb191830f7dab0803f0e080022b91dd1c
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-26 22:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (83.72%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.js
+++ 101-symmetric-tree/symmetric-tree.js
@@ -18,10 +18,8 @@ var isSymmetric = function (root) {
         if (!p1 && !p2) continue;
         if (!p1 || !p2) return false;
         if (p1.val !== p2.val) return false;
-        q.push(p1.left);
-        q.push(p2.right);
-        q.push(p1.right);
-        q.push(p2.left);
+        q.push(p1.left, p2.right);
+        q.push(p1.right, p2.left);
     }
     return true;
 };
