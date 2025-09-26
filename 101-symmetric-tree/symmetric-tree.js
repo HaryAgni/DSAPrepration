eb93e252a3462cf439495c302b2845a55fd39458
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-26 22:46:43 +0530

    Time: 2 ms (5.10%) | Memory: 56 MB (87.90%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.js
+++ 101-symmetric-tree/symmetric-tree.js
@@ -11,12 +11,17 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-    var traversal = function (leftNode, rightNode) {
-        if (!leftNode && !rightNode) return true;
-        if (!leftNode || !rightNode) return false;
-        return leftNode.val == rightNode.val &&
-            traversal(leftNode.left, rightNode.right) &&
-            traversal(leftNode.right, rightNode.left);
+    let q = [root.left, root.right];
+    while (q.length) {
+        let p1 = q.shift();
+        let p2 = q.shift();
+        if (!p1 && !p2) continue;
+        if (!p1 || !p2) return false;
+        if (p1.val !== p2.val) return false;
+        q.push(p1.left);
+        q.push(p2.right);
+        q.push(p1.right);
+        q.push(p2.left);
     }
-    return traversal(root.left, root.right);
+    return true;
 };
