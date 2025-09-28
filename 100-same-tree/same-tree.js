8e50ef6f18d1adee9f7e8c32e3120cab83baf8e9
Harshit Agnihotri <haryagni123@gmail.com>
2025-09-28 23:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (20.21%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -16,4 +16,5 @@ var isSameTree = function (p, q) {
     if (!p || !q) return false;
     if (p.val !== q.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    
 };
