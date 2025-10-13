b2ecc9b6712634e7577e46877e37bb15510205f3
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-13 23:12:39 +0530

    Time: 474 ms (16.58%) | Memory: 87.3 MB (45.34%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -9,12 +9,13 @@ var maxSlidingWindow = function (nums, k) {
 
     let i = 0;
     for (let j = 0; j < nums.length; j++) {
-        
+
         while (q.length && q[q.length - 1] < nums[j]) q.pop();
         q.push(nums[j]);
         if (j >= k - 1) {
             ans.push(q[0]);
             q[0] == nums[i] && q.shift();
+            
             i++;
         }
     }
