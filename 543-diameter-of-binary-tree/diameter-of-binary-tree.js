44e9e70c7696dffca4191308087b090b224eff5f
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-22 22:01:19 +0530

    Time: 2 ms (32.73%) | Memory: 61.5 MB (70.92%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -20,6 +20,7 @@ var diameterOfBinaryTree = function (root) {
         maxDiameter = Math.max(currDiameter, maxDiameter);
         return 1 + Math.max(left, right);
     }
+    
     traversal(root);
     return maxDiameter;
 };
