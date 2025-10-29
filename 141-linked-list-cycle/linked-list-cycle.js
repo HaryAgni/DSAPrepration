01593599f9657de82fbc476b42636f447ea4f5d2
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-29 17:26:04 +0530

    Time: 59 ms (44.23%) | Memory: 59.7 MB (39.14%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,13 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function (head) {
-    if(!head) return false;
-    let set = new Set();
-    let curr = head;
-    while (curr) {
-        if (set.has(curr)) return true;
-        set.add(curr);
-        curr = curr.next;
+    if (!head || !head.next) return false;
+    let slow = head;
+    let fast = head.next;
+    while (slow && fast && fast.next) {
+        if (slow == fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
     }
     return false;
 };
