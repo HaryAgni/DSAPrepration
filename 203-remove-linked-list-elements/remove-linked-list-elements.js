e4f0f7fa0d6f70a4d0292de1cafda058827c4ddd
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-29 18:53:23 +0530

    Time: 1 ms (55.84%) | Memory: 60.1 MB (70.56%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.js
+++ 203-remove-linked-list-elements/remove-linked-list-elements.js
@@ -11,13 +11,19 @@
  * @return {ListNode}
  */
 var removeElements = function (head, val) {
-    let dummy = new ListNode();
-    dummy.next = head;
+    let dummy = new ListNode(null, head);
     let prev = dummy;
-    while (prev && prev.next) {
-        if (prev.next.val == val) prev.next = prev.next.next;
-        else prev = prev.next;
-
+    let curr = head;
+    while (prev && curr) {
+        if (curr.val == val) {
+            prev.next = curr.next;
+            curr.next = null;
+            curr = prev.next;
+        }
+        else {
+            curr = curr.next;
+            prev = prev.next;
+        }
     }
     return dummy.next;
 };
