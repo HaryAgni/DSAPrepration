400cd3975cfce9c6758268c2f1462670dc4575da
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-29 15:20:01 +0530

    Time: 5 ms (35.28%) | Memory: 59 MB (8.83%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    let sum = 0;
-    let n = nums.length;
-    for (let i = 0; i < n; i++) {
-        sum += nums[i];
+    let set = new Set(nums);
+    for (let i = 0; i <= nums.length; i++) {
+        if (!set.has(i)) return i;
     }
-    return (n * (n + 1) / 2) - sum;
 };
