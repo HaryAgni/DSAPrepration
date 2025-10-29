4db2f6a32639edc8192d153b3ff83fc808797bf1
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-30 00:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (48.31%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    let wordCount = 0;
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (wordCount > 0 && s[i] == " ") break;
-        else if (s[i] !== ' ') wordCount++;
+    let len = 0;
+    for (let i = s.length - 1; i >=0; i--) {
+        if (s[i] !== " ") len++;
+        else if (s[i] == " " && len) break;
     }
-    return wordCount;
+    return len;
 };
