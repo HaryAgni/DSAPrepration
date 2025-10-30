18d9b4817592fb4e896dadce40f636e83e28824b
Harshit Agnihotri <haryagni123@gmail.com>
2025-10-30 13:26:30 +0530

    Time: 16 ms (28.91%) | Memory: 58.5 MB (43.42%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -4,14 +4,12 @@
  */
 var lengthOfLongestSubstring = function (s) {
     let map = {};
-    let maxWindow = 0;
     let i = 0;
-    let j = 0;
-    while (j < s.length) {
+    let maxWindow = 0;
+    for (let j = 0; j < s.length; j++) {
         if (s[j] in map && map[s[j]] >= i) i = map[s[j]] + 1;
         map[s[j]] = j;
         maxWindow = Math.max(maxWindow, j - i + 1);
-        j++;
     }
     return maxWindow;
 };
