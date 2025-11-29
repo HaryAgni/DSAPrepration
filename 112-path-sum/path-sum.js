f5e63fe3f7f7a0db80b9d7b392fa147892f786b4
Harshit Agnihotri <haryagni123@gmail.com>
2025-11-29 23:02:01 +0530

    Time: 1 ms (46.46%) | Memory: 56.7 MB (79.76%) - LeetSync

--- 112-path-sum/path-sum.js
+++ 112-path-sum/path-sum.js
@@ -13,6 +13,7 @@
  */
 var hasPathSum = function (root, targetSum) {
     if (!root) return false;
+    
     if (!root.left && !root.right && root.val == targetSum) return true;
     let left = hasPathSum(root.left, targetSum - root.val);
     let right = hasPathSum(root.right, targetSum - root.val);
