bbb263aa2850fd604d28071f6d15f1c220083e12
Harshit Agnihotri <haryagni123@gmail.com>
2025-12-06 23:00:29 +0530

    Time: 128 ms (5.42%) | Memory: 54.1 MB (6.10%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -2,13 +2,11 @@ import java.math.BigInteger;
 
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        
+
       ArrayList<Integer> mylist = new ArrayList<>();
         String str = "";
-
         for(int i =0;i<num.length;i++){
             str = str + num[i];
-            
         }
         BigInteger ans = new BigInteger(str);
         BigInteger res = ans.add(BigInteger.valueOf(k));
