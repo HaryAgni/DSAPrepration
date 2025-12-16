65258cde2a34e496fe4544e734070d65c39056aa
Harshit Agnihotri <haryagni123@gmail.com>
2025-12-16 23:21:35 +0530

    Fix formatting issue in isSymmetric function

--- 101-symmetric-tree/symmetric-tree.js
+++ 101-symmetric-tree/symmetric-tree.js
@@ -11,7 +11,7 @@
  * @return {boolean}
  */
 var isSymmetric = function (root) {
-    let q = [root.left, root.right];
+   let q = [root.left, root.right];
     while (q.length) {
         let p1 = q.shift();
         let p2 = q.shift();
