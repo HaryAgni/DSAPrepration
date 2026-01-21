2b9052c4f233861f5cfd85dd68c63a799ac5eaa2
Harshit Agnihotri <haryagni123@gmail.com>
2026-01-21 21:45:57 +0530

    Time: 2 ms (81.47%) | Memory: 64.3 MB (91.98%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -1,13 +1,13 @@
 /**
- * @param {number[]} nums
+ * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (nums) {
+var maxProfit = function (prices) {
     let maxProfit = 0;
-    let min = nums[0];
-    for (let i = 0; i < nums.length; i++) {
-        min = Math.min(min, nums[i]);
-        maxProfit = Math.max(maxProfit, nums[i] - min);
+    let minPrice = prices[0];
+    for (let i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
     }
     return maxProfit;
 };
