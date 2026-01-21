005abe1349e8dabd270719c02facc1bb446d51a0
Harshit Agnihotri <haryagni123@gmail.com>
2026-01-21 21:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 56.5 MB (93.98%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,7 +4,7 @@
  */
 var removeDuplicates = function (nums) {
     let i = 0;
-    for (let j = 0; j < nums.length; j++) {
+    for (let j = 1; j < nums.length; j++) {
         if (nums[i] !== nums[j]) {
             nums[i + 1] = nums[j];
             i++;
