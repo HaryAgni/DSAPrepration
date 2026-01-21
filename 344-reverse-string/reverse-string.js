21f67365bf5e6dee10141697979e699b23419d5b
Harshit Agnihotri <haryagni123@gmail.com>
2026-01-21 21:35:23 +0530

    Time: 1 ms (57.73%) | Memory: 60.1 MB (83.89%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,13 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-    let left = 0;
-    let right = s.length - 1;
-    while (left < right) {
-        let temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-        left++;
-        right--;
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
     }
 };
