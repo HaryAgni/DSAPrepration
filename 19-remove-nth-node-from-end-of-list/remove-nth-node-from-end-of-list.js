2c41ab90b75193772e49403ad57d1724495caf3e
Harshit Agnihotri <haryagni123@gmail.com>
2026-01-24 21:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (75.57%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -12,12 +12,12 @@
  */
 var removeNthFromEnd = function (head, n) {
     let dummy = new ListNode(null, head);
-    let slow = dummy;
     let fast = dummy;
-    for (i = 0; i <= n; i++) {
+    let slow = dummy;
+    for (let i = 0; i < n; i++) {
         fast = fast.next;
     }
-    while (fast) {
+    while (fast && fast.next) {
         slow = slow.next;
         fast = fast.next;
     }
