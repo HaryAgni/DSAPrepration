7e4c53ffa4927626d30984c97815b1392c463733
Harshit Agnihotri <haryagni123@gmail.com>
2026-01-30 00:00:14 +0530

    Time: 1 ms (95.83%) | Memory: 54.7 MB (98.61%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -7,7 +7,7 @@ var reverseStr = function (s, k) {
     s = s.split('')
     for (let i = 0; i < s.length; i = i + (k * 2)) {
         let l = i;
-        let r = ((k + i - 1) >= s.length) ? s.length - 1 : (k + i - 1);
+        let r = Math.min(i + k - 1, s.length - 1);
         while (l < r) {
             let temp = s[l];
             s[l] = s[r];
