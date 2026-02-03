f8b2ceae921d5437669dc1071805efdbb6adf7c4
Harshit Agnihotri <haryagni123@gmail.com>
2026-02-03 20:42:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (87.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    let map = { '}': '{', ')': '(', ']': '[' };
+    let map = { '(': ')', '{': '}', '[': ']' };
     let stack = [];
     for (let i = 0; i < s.length; i++) {
-        if (!map[s[i]]) stack.push(s[i]);
-        else if (stack.pop() !== map[s[i]]) return false;
+        if (map[s[i]]) stack.push(s[i]);
+        else if (map[stack.pop()] !== s[i]) return false;
     }
-    return stack.length == 0;
+    return stack.length==0;
 };
