49f342b68abe91d447b00de70882ce122e7b76c9
Harshit Agnihotri <haryagni123@gmail.com>
2026-02-04 00:30:50 +0530

    Time: 4 ms (58.34%) | Memory: 56.1 MB (65.37%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,17 +3,12 @@
  * @return {string}
  */
 var removeOuterParentheses = function (s) {
+    let ans = "";
     let count = 0;
-    let res = "";
     for (let i = 0; i < s.length; i++) {
-        if (s[i] == '(') {
-            count++;
-            if (count > 1) res += s[i];
-        }
-        else {
-            count--;
-            if (count) res += s[i];
-        }
+        if (s[i] == '(') count++;
+        else count--;
+        if ((count > 1 && s[i] == '(') || (count !== 0 && s[i] == ')')) ans += s[i];
     }
-    return res;
+    return ans;
 };
