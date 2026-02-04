d16835eccfcb5b19fbea1a2ec2106075f58d4412
Harshit Agnihotri <haryagni123@gmail.com>
2026-02-04 23:42:12 +0530

    Time: 2 ms (86.77%) | Memory: 57.8 MB (15.91%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -5,6 +5,7 @@
 var removeOuterParentheses = function (s) {
     let ans = "";
     let count = 0;
+    
     for (let i = 0; i < s.length; i++) {
         if (s[i] == '(') count++;
         else count--;
