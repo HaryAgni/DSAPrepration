be100c57b3465aa0d5906cd490dff0783afa3dbb
Harshit Agnihotri <haryagni123@gmail.com>
2026-02-10 00:10:57 +0530

    Time: 4 ms (88.25%) | Memory: 57.3 MB (81.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -3,37 +3,37 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
-    let n = grid.length;
-    let m = grid[0].length;
+    let m = grid.length;
+    let n = grid[0].length;
     let q = [];
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (grid[i][j] == 2) q.push([i, j, 0]);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] == 2) q.push([i, j, 0])
         }
     }
     let minutes = 0;
     while (q.length) {
-        let [x, y, min] = q.shift();
-        if (x - 1 >= 0 && grid[x - 1][y] == 1) {
-            grid[x - 1][y] = 2;
-            q.push([x - 1, y, min + 1]);
+        let [i, j, l] = q.shift();
+        if (i > 0 && grid[i - 1][j] == 1) {
+            grid[i - 1][j] = 2;
+            q.push([i - 1, j, l + 1]);
         }
-        if (x + 1 < n && grid[x + 1][y] == 1) {
-            grid[x + 1][y] = 2;
-            q.push([x + 1, y, min + 1]);
+        if (i < m - 1 && grid[i + 1][j] == 1) {
+            grid[i + 1][j] = 2;
+            q.push([i + 1, j, l + 1]);
         }
-        if (y - 1 >= 0 && grid[x][y - 1] == 1) {
-            grid[x][y - 1] = 2;
-            q.push([x, y - 1, min + 1]);
+        if (j > 0 && grid[i][j - 1] == 1) {
+            grid[i][j - 1] = 2;
+            q.push([i, j - 1, l + 1]);
         }
-        if (y + 1 < m && grid[x][y + 1] == 1) {
-            grid[x][y + 1] = 2;
-            q.push([x, y + 1, min + 1]);
+        if (j < n - 1 && grid[i][j + 1] == 1) {
+            grid[i][j + 1] = 2;
+            q.push([i, j + 1, l + 1]);
         }
-        minutes = min;
+        minutes = Math.max(l, minutes);
     }
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
             if (grid[i][j] == 1) return -1;
         }
     }
