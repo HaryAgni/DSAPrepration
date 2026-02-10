79aaffa64ddf57fc865bc976bde9b50a7e384a49
Harshit Agnihotri <haryagni123@gmail.com>
2026-02-10 13:11:36 +0530

    Time: 1 ms (73.52%) | Memory: 55.4 MB (86.86%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-    if (x <= 1) return x;
-    let start = 2;
-    let end = Math.floor(x / 2);
-    while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
+    let l = 0;
+    let r = Math.ceil(x / 2);
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
         if (mid * mid == x) return mid;
-        else if (mid * mid > x) end = mid - 1;
-        else start = mid + 1;
+        else if (mid * mid < x) l = mid + 1;
+        else r = mid - 1;
     }
-    return end;
+    return r;
 };
