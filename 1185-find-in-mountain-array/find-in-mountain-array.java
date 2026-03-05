bc326ada903f8818760682577b6828c3abbc41f1
Harshit Agnihotri <haryagni123@gmail.com>
2026-03-05 23:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (60.92%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -7,6 +7,7 @@
  * }
  */
 class Solution {
+
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int peak = findPeakElement(mountainArr);
         int firstTry = binarySearch(mountainArr,target,0,peak);
