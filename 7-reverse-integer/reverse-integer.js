464b519ff79f5d85e9a12561d5cf0f3cc4214996
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-13 23:43:58 +0530

    Time: 57 ms (31.40%) | Memory: 54.8 MB (94.26%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -4,12 +4,12 @@
  */
 var reverse = function (x) {
     let temp = Math.abs(x);
-    let reverse = 0;
-    while (temp > 0) {
-        reverse = (reverse * 10) + (temp % 10);
-        temp = Math.floor(temp / 10);
-
+    let reverse = "";
+     while(temp>0){
+        reverse += temp%10;
+        temp = Math.floor(temp/10);
     }
+    reverse = Number(reverse);
     let limit = Math.pow(2, 31);
     if (reverse < -limit || reverse > (limit - 1)) return 0;
     return x < 0 ? -reverse : reverse;
