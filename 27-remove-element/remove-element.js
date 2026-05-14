437ed7031d03df29f65c42089ec381e89f55c14b
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-14 23:25:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (52.50%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -5,11 +5,13 @@
  */
 var removeElement = function (nums, val) {
     let i = 0;
-    for (let j = 0; j < nums.length; j++) {
+    let j = 0;
+    while (j < nums.length) {
         if (nums[j] !== val) {
             nums[i] = nums[j];
             i++;
         }
+        j++;
     }
     return i;
 };
