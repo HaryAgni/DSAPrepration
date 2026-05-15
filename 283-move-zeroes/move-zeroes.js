642122a1e3468ca523fcede44a3f13448d299198
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-15 11:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 60.2 MB (66.50%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -6,6 +6,7 @@ var moveZeroes = function (nums) {
     let i = 0;
     for (let j = 0; j < nums.length; j++) {
         if (nums[j] !== 0) {
+            
             let temp = nums[i];
             nums[i] = nums[j];
             nums[j] = temp;
