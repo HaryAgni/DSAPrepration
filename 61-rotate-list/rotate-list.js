e7d8b60b59e7bb0ff8678642aa99e80f79a33871
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-22 13:42:50 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (69.68%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -19,19 +19,18 @@ var rotateRight = function (head, k) {
         curr = curr.next;
     }
     k = k % size;
-    if (k == 0) return head;
-    let fast = head;
+    if (size == k || !k) return head;
     let slow = head;
+    let fast = head;
     for (let i = 0; i < k; i++) {
         fast = fast.next;
     }
-    while (fast.next) {
-        fast = fast.next;
+    while (fast && fast.next) {
         slow = slow.next;
+        fast = fast.next;
     }
     let temp = slow.next;
     slow.next = null;
     fast.next = head;
     return temp;
-
 };
