3c3b734149fe14650c88044a836dbfe5ce35d9d2
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-22 14:02:10 +0530

    Time: 19 ms (4.19%) | Memory: 62.3 MB (5.90%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var findWordsContaining = function (words, x) {
-    let res = [];
+    let ans = [];
     for (let i = 0; i < words.length; i++) {
-        let set = new Set(words[i]);
-        if (set.has(x)) res.push(i);
+        let word = words[i];
+        let set = new Set(word);
+        if (set.has(x)) ans.push(i);
     }
-    return res;
-
+    return ans;
 };
