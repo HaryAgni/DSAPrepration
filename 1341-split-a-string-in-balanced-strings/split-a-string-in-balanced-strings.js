66072e06784df2e9306a3fc19ae9ce5aaf7f2d81
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-26 11:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (87.98%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -6,7 +6,7 @@ var balancedStringSplit = function (s) {
     let ans = 0;
     let count = 0;
     for (let i = 0; i < s.length; i++) {
-        if (s[i] == 'L') count++;
+        if (s[i] == 'R') count++;
         else count--;
         if (count == 0) ans++;
     }
