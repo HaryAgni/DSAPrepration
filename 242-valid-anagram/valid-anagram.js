4bb3494b7014a47a5cedb15117ed86874fdb6e22
Harshit Agnihotri <haryagni123@gmail.com>
2026-05-26 15:01:57 +0530

    Time: 14 ms (56.69%) | Memory: 56.3 MB (66.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,16 +5,15 @@
  */
 var isAnagram = function (s, t) {
     if (s.length !== t.length) return false;
-    let m1 = {};
-    let m2 = {};
+    let sMap = {};
+    let tMap = {};
     for (let i = 0; i < s.length; i++) {
-        m1[s[i]] = m1[s[i]] ? ++m1[s[i]] : 1;
-        m2[t[i]] = m2[t[i]] ? ++m2[t[i]] : 1;
+        sMap[s[i]] = sMap[s[i]] ? ++sMap[s[i]] : 1;
+        tMap[t[i]] = tMap[t[i]] ? ++tMap[t[i]] : 1;
     }
-    let keys = Object.keys(m1);
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        if (!m2[key] || m1[key] !== m2[key]) return false;
+    for (const key in sMap) {
+        if (!tMap[key] || sMap[key] !== tMap[key]) return false;
     }
     return true;
+
 };
