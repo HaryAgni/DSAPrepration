b853afe995d80a194adca94dd5204f3bd167d1d5
Harshit Agnihotri <haryagni123@gmail.com>
2026-06-01 23:04:13 +0530

    Time: 7 ms (60.51%) | Memory: 62.1 MB (58.97%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -3,20 +3,19 @@
  * @return {number[]}
  */
 var nextGreaterElements = function (nums) {
-    let ans = Array(nums.length).fill(-1);
     let stack = [];
-    for (let i = nums.length * 2 - 1; i >= 0; i--) {
+    let ans = Array(nums.length).fill(-1);
+    for (let i = (nums.length * 2) - 1; i >= 0; i--) {
         let index = i % nums.length;
-        let val = nums[index];
         while (stack.length) {
             let top = stack[stack.length - 1];
-            if (top > val) {
+            if (top > nums[index]) {
                 ans[index] = top;
                 break;
             }
             else stack.pop();
         }
-        stack.push(val);
+        stack.push(nums[index]);
     }
     return ans;
-}
+};
