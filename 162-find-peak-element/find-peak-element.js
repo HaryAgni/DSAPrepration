8e09d6cdf05b3956d6c1ed50c088eae8d96b637a
Harshit Agnihotri <haryagni123@gmail.com>
2026-06-04 16:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (51.24%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-    let start = 0;
-    let end = nums.length - 1;
-    while (start < end) {
-        let mid = start + Math.floor((end - start) / 2);
-        if (nums[mid] > nums[mid + 1]) end = mid;
-        else start = mid + 1;
+    let l = 0;
+    let r = nums.length - 1;
+    while (l < r) {
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] > nums[mid + 1]) r = mid;
+        else l = mid + 1;
     }
-    return end;
+    return r;
 };
