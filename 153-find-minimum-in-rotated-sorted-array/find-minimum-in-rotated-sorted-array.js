cb0551ca62dd62e76e7bb248de84735c520bf84b
Harshit Agnihotri <haryagni123@gmail.com>
2026-06-04 17:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (87.41%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -9,7 +9,7 @@ var findMin = function (nums) {
         let mid = Math.floor((l + r) / 2);
         if (nums[l] <= nums[r]) return nums[l];
         else if (nums[mid] > nums[mid + 1]) return nums[mid + 1];
-        else if (nums[mid] > nums[l]) l = mid;
+        else if (nums[mid] > nums[l]) l = mid + 1;
         else r = mid;
     }
     return nums[l];
