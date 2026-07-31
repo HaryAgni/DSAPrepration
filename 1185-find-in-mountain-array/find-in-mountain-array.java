f40ebc8d740365206cb4b268da4e99f4e8174c6d
Harshit Agnihotri <haryagni123@gmail.com>
2026-07-31 23:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (8.41%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -5,8 +5,7 @@
  *     public int get(int index) {}
  *     public int length() {}
  * }
- */ 
- 
+ */
 class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int peak = findPeakElement(mountainArr);
